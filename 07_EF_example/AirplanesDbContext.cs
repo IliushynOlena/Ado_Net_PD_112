d710c8ba93c9ec9a0ac544189c06051b727b5bcc
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-02-22 17:21:51 +0200

    Seed data

--- 07_EF_example/AirplanesDbContext.cs
+++ 07_EF_example/AirplanesDbContext.cs
@@ -40,48 +40,48 @@ namespace _07_EF_example
             base.OnModelCreating(modelBuilder);
 
             //Initializer - Seeder
-            //modelBuilder.Entity<Airplane>().HasData(new Airplane[]
-            //{
-            //    new Airplane()
-            //    {
-            //        Id = 1,
-            //        Model = "Antonov 125",
-            //        MaxPassangers = 1200
-            //    },
-            //    new Airplane()
-            //    {
-            //        Id = 2,
-            //        Model = "Boeing 747",
-            //        MaxPassangers = 1300
-            //    }
-            //});
+            modelBuilder.Entity<Airplane>().HasData(new Airplane[]
+            {
+                new Airplane()
+                {
+                    Id = 1,
+                    Model = "Antonov 125",
+                    MaxPassangers = 1200
+                },
+                new Airplane()
+                {
+                    Id = 2,
+                    Model = "Boeing 747",
+                    MaxPassangers = 1300
+                }
+            });
 
-            //modelBuilder.Entity<Flight>().HasData(new Flight[]
-            //{
-            //    new Flight()
-            //    {
-            //        Number = 1,
-            //        AirplaneId = 1,
-            //        ArrivalCity = "Lviv",
-            //        DepartureCity = "Kyiv",
-            //        DepartureTime = new DateTime(2023,3,12),
-            //        ArrivalTime = new DateTime(2023,3,12)
+            modelBuilder.Entity<Flight>().HasData(new Flight[]
+            {
+                new Flight()
+                {
+                    Number = 1,
+                    AirplaneId = 1,
+                    ArrivalCity = "Lviv",
+                    DepartureCity = "Kyiv",
+                    DepartureTime = new DateTime(2023,3,12),
+                    ArrivalTime = new DateTime(2023,3,12)
 
-            //    },
-            //    new Flight()
-            //    {
-            //        Number = 2,
-            //        AirplaneId = 2,
-            //        ArrivalCity = "Lviv",
-            //        DepartureCity = "Warsaw",
-            //        DepartureTime = new DateTime(2023,5,12),
-            //        ArrivalTime = new DateTime(2023,5,12)
+                },
+                new Flight()
+                {
+                    Number = 2,
+                    AirplaneId = 2,
+                    ArrivalCity = "Lviv",
+                    DepartureCity = "Warsaw",
+                    DepartureTime = new DateTime(2023,5,12),
+                    ArrivalTime = new DateTime(2023,5,12)
 
-            //    }
-            //});
-           
+                }
+            });
 
-           
+
+
         }
     }
 }
---
+++ 07_EF_example/Migrations/20230222152127_SeedData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _07_EF_example.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Airplanes",
+                columns: new[] { "Id", "MaxPassangers", "Model" },
+                values: new object[] { 1, 1200, "Antonov 125" });
+
+            migrationBuilder.InsertData(
+                table: "Airplanes",
+                columns: new[] { "Id", "MaxPassangers", "Model" },
+                values: new object[] { 2, 1300, "Boeing 747" });
+
+            migrationBuilder.InsertData(
+                table: "Flights",
+                columns: new[] { "Number", "AirplaneId", "ArrivalCity", "ArrivalTime", "DepartureCity", "DepartureTime", "Rating" },
+                values: new object[] { 1, 1, "Lviv", new DateTime(2023, 3, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Kyiv", new DateTime(2023, 3, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.InsertData(
+                table: "Flights",
+                columns: new[] { "Number", "AirplaneId", "ArrivalCity", "ArrivalTime", "DepartureCity", "DepartureTime", "Rating" },
+                values: new object[] { 2, 2, "Lviv", new DateTime(2023, 5, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Warsaw", new DateTime(2023, 5, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Flights",
+                keyColumn: "Number",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Flights",
+                keyColumn: "Number",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Airplanes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Airplanes",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
